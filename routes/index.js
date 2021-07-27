69d1e167d452d3e845806dfcb0d06e2e328b82f9
Cesar Jr.Fontalvo <juniorfontalvo5@gmail.com>
2021-07-27 10:35:23 -0500

    creando las rutas

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,10 @@ const express = require('express')
 //const userCtrl = require('../controllers/user')
 
 const api = express.Router()
+
+
+
+api.post('/signup', userCtrl.signUp)
+api.post('/signin', userCtrl.signIn)
+
+module.exports = api
